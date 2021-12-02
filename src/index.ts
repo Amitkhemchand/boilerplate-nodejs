1b04001ab1e5879a677620f7c2f18b1727ef05f0
amitkhemchand <amitkhemchand@gmail.com>
2021-12-02 09:26:52 +0530

    add commit

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express from 'express'
 
 const prisma = new PrismaClient()
 
-
+// add new comment
 const app = express()
 
 app.use(express.json())
@@ -13,6 +13,6 @@ app.get('/api', async (req, res) => {
     res.send('Hello World!');
 })
 
-app.listen(3000, () =>
-  console.log('REST API server ready at: http://localhost:3000'),
+app.listen(3002, () =>
+  console.log('REST API server ready at: http://localhost:3002'),
 )
